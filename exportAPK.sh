e0a1cf7b962d51deae8b5fe4fdfbb9e54e0c9e31
hayukleung <hayukleung@gmail.com>
2024-04-10 13:52:06 +0800

    modify: export apk from android device to pc

--- exportAPK.sh
+++ exportAPK.sh
@@ -2,3 +2,20 @@
 
 # go to directory of command
 cd "$(dirname "$0")"
+
+if [ 1 == $# ]; then
+    adb pull $(adb shell pm path $(./showCurrentPackageName.sh) | awk -F "package:" '{print $2}') $1
+    exit 1
+fi
+
+if [ 3 == $# ]; then
+    if [ '-s' == $1 ]; then
+        adb pull $(adb -s $2 shell pm path $(./showCurrentPackageName.sh $1 $2) | awk -F "package:" '{print $2}') $3
+        exit 1
+    fi
+fi
+
+echo 'execute like this: '
+echo '$ exportAPK.sh $targetPath'
+echo 'or'
+echo '$ exportAPK.sh -s $device $targetPath'
--- showCurrentActivity.sh
+++ showCurrentActivity.sh
@@ -4,15 +4,15 @@
 cd "$(dirname "$0")"
 
 if [ 0 == $# ]; then
-    adb shell dumpsys activity | grep "mResume" | awk '{printf $4}'            # <= android 12
-    adb shell dumpsys activity | grep "topResumedActivity" | awk '{printf $3}' # >= android 13
+    adb shell dumpsys activity | grep "mResume"            | awk '{printf $4}' | awk -F "}" '{print $1}' # <= android 12
+    adb shell dumpsys activity | grep "topResumedActivity" | awk '{printf $3}' | awk -F "}" '{print $1}' # >= android 13
     exit 1
 fi
 
 if [ 2 == $# ]; then
     if [ '-s' == $1 ]; then
-        adb -s $2 shell dumpsys activity | grep "mResume" | awk '{printf $4}'            # <= android 12
-        adb -s $2 shell dumpsys activity | grep "topResumedActivity" | awk '{printf $3}' # >= android 13
+        adb -s $2 shell dumpsys activity | grep "mResume"            | awk '{printf $4}' | awk -F "}" '{print $1}' # <= android 12
+        adb -s $2 shell dumpsys activity | grep "topResumedActivity" | awk '{printf $3}' | awk -F "}" '{print $1}' # >= android 13
         exit 1
     fi
 fi
