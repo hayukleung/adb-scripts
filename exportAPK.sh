f9fd87e9e7dcc403d3fd0aa58541795c9b2846ea
hayukleung <hayukleung@gmail.com>
2024-04-12 15:56:28 +0800

    modify: export apk from android device to pc

--- exportAPK.sh
+++ exportAPK.sh
@@ -4,13 +4,19 @@
 cd "$(dirname "$0")"
 
 if [ 1 == $# ]; then
+    if [ ! -e $1 ]; then
+        mkdir -p $1
+    fi
     adb pull $(adb shell pm path $(./showCurrentPackageName.sh) | awk -F "package:" '{print $2}') $1
     exit 1
 fi
 
 if [ 3 == $# ]; then
     if [ '-s' == $1 ]; then
-        adb pull $(adb -s $2 shell pm path $(./showCurrentPackageName.sh $1 $2) | awk -F "package:" '{print $2}') $3
+        if [ ! -e $3 ]; then
+            mkdir -p $3
+        fi
+        adb -s $2 pull $(adb -s $2 shell pm path $(./showCurrentPackageName.sh $1 $2) | awk -F "package:" '{print $2}') $3
         exit 1
     fi
 fi
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # Scripts for ADB usage
+
+## May Help You
+
+https://apk.support/download-app/com.aefyr.sai
+
+这个 app 可以将谷歌商店拆分的 apk 合并一起安装
