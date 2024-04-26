eb1f8f695526130d358a1e53991010df18378bba
hayukleung <hayukleung@gmail.com>
2024-04-26 17:26:17 +0800

    modify: home

--- home.sh
+++ home.sh
@@ -3,19 +3,4 @@
 # go to directory of command
 cd "$(dirname "$0")"
 
-if [ 0 == $# ]; then
-    adb shell input keyevent 3
-    exit 1
-fi
-
-if [ 2 == $# ]; then
-    if [ '-s' == $1 ]; then
-        adb -s $2 shell input keyevent 3
-        exit 1
-    fi
-fi
-
-echo 'execute like this: '
-echo '$ home'
-echo 'or'
-echo '$ home -s $device'
+./devices "shell input keyevent 3"
